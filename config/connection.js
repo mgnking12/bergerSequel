e18ec5c199ef5c3c38e4d96beee58de02345e66c
Megan King <mgnking12@gmail.com>
2016-08-04 15:43:14 -0500

    heroku

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,23 @@
 var mysql = require('mysql');
 var connection = mysql.createConnection(process.env.JAWSDB_URL);
-var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'burgers_db'
-});
+
+if (process.env.JAWSDB_URL) {
+    // connection = mysql.createConnection(process.env.JAWSDB_URL);
+    connection = mysql.createConnection({
+        port: 3306,
+        host: 'gx97kbnhgjzh3efb.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        user: 'foywabjkwudkjdlb',
+        password: 'svmstfvicef4obe3',
+        database: 'buhsre8sxi7nltel'
+    });
+} else {
+    connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'burgers_db'
+    });
+}
 
 
 connection.connect(function(err) {
